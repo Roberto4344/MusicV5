1c37b2b2518c896731168a9881431d518d398e63
Roberto <robertoabad93@gmail.com>
2021-02-04 13:07:41 +0100

    se ha modificado para añadir un metodo que encuentra la cancion por artista

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -170,4 +170,16 @@ public class MusicOrganizer
             addTrack(track);
         }
     }
+    
+     /**
+     * Enumera todas las pistas que contengan  la cadena buscada.
+     */
+    public void findInTitle(String searchString){
+        for(Track track : tracks){
+            String title = track.getTitle();
+            if(title.contains(searchString)){
+                System.out.println(track.getDetails());
+            }
+        }
+    }
 }
